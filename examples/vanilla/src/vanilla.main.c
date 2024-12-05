b82d4b81062051886c378730132e5d73623a77be
Gtker <git@gtker.com>
2024-12-05 16:52:21 +0100

    Refactor example

--- examples/vanilla/src/vanilla.main.c
+++ examples/vanilla/src/vanilla.main.c
@@ -1,138 +1,68 @@
-#if defined(_MSC_VER)
-#define WIN32_LEAN_AND_MEAN
-#include <basetsd.h>
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-typedef SSIZE_T ssize_t;
-typedef int socklen_t;
-typedef SOCKET fd_type;
-typedef char socket_opt_type;
-typedef int socket_length_type;
-
-#define close closesocket
-#define WINDOWS_INT_CAST (int)
-
-#else
-#include <netdb.h>
-#include <sys/socket.h>
-#include <unistd.h>
-
-typedef int fd_type;
-typedef int socket_opt_type;
-typedef int socket_length_type;
-#define WINDOWS_INT_CAST
-#endif
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
+#include "server_socket.h"
 
 #include "wow_login_messages/all.h"
 #include "wow_login_messages/version3.h"
 #include "wow_srp/server.h"
 #include "wow_srp/wow_srp.h"
 
-#define LISTEN_BACKLOG 10
-
 static uint8_t GENERATOR = {WOW_SRP_GENERATOR};
+
+#define BUFFER_SIZE 4096
+static unsigned char buffer[BUFFER_SIZE];
+
+void expect_version3_opcode(WowLoginReader* reader,
+                            ServerSocket* sock,
+                            Version3ClientOpcodeContainer* opcodes,
+                            WowLoginOpcode opcode)
+{
+    reader->index = 0;
+    reader->length = server_recv(sock, buffer, BUFFER_SIZE);
+    WowLoginResult result = version3_client_opcode_read(reader, opcodes);
+
+    if (result != WLM_RESULT_SUCCESS)
+    {
+        puts("failed to read opcodes v3");
+        abort();
+    }
+
+    if (opcodes->opcode != opcode)
+    {
+        printf("incorrect opcodes received, expected %d, received %d", opcode, opcodes->opcode);
+        abort();
+    }
+}
+
+void write_version3_opcode(WowLoginWriter* writer, ServerSocket* sock, Version3ServerOpcodeContainer* opcodes)
+{
+    writer->index = 0;
+    version3_server_write_opcode(writer, opcodes);
+    server_send(sock, buffer, writer->index + 1);
+}
+
+static version3_Realm realms = {VERSION2_REALM_TYPE_PLAYER_VS_ENVIRONMENT,
+                                VERSION2_REALM_FLAG_NONE,
+                                {sizeof("Test Name"), "Test Name"},
+                                {sizeof("localhost"), "localhost"},
+                                0.0f,
+                                2,
+                                VERSION2_REALM_CATEGORY_DEFAULT,
+                                1};
 
 int main(void)
 {
-    struct addrinfo hints = {0};
-    struct addrinfo* res = NULL;
-    fd_type sockfd = 0;
-    fd_type result = 0;
-    fd_type clientfd = 0;
-    struct sockaddr_storage storage = {0};
-    socklen_t socklen = sizeof(storage);
     int ret = 0;
 
-#if defined(_MSC_VER)
-    WSADATA wsaData;
+    ServerSocket sock = initialize_socket("3724");
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        fprintf(stderr, "WSAStartup failed.\n");
-        exit(1);
-    }
-
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-    {
-        fprintf(stderr, "Versiion 2.2 of Winsock is not available.\n");
-        WSACleanup();
-        exit(2);
-    }
-#endif
-
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;  // fill in my IP for me
-
-    result = getaddrinfo(NULL, "3724", &hints, &res);
-    if (result == -1)
-    {
-        puts("getaddrinfo returned failure.");
-        ret = 1;
-        goto exit_goto;
-    }
-
-    puts("Binding to 3724");
-
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-    // bind it to the port we passed in to getaddrinfo():
-
-    socket_opt_type one = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
-
-    result = bind(sockfd, res->ai_addr, WINDOWS_INT_CAST res->ai_addrlen);
-    if (result == -1)
-    {
-        puts("bind failed.");
-        ret = 1;
-        goto exit_goto;
-    }
-
-    result = listen(sockfd, LISTEN_BACKLOG);
-    if (result == -1)
-    {
-        puts("listen failed.");
-        ret = 1;
-        goto exit_goto;
-    }
-
-    clientfd = accept(sockfd, (struct sockaddr*)&storage, &socklen);
-    if (clientfd == -1)
-    {
-        puts("accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    unsigned char buf[512] = {0};
-    ssize_t bytes = recv(clientfd, (char*)buf, 512, 0);
-    if (bytes == -1)
-    {
-        puts("recv failed");
-        ret = 1;
-        goto exit_goto;
-    }
+    size_t bytes = server_recv(&sock, buffer, BUFFER_SIZE);
 
     AllClientOpcodeContainer opcodes;
-    WowLoginReader reader = wlm_create_reader(buf, bytes);
+    WowLoginReader reader = wlm_create_reader(buffer, bytes);
 
     WowLoginResult r = all_client_opcode_read(&reader, &opcodes);
-    if (r != WLM_RESULT_SUCCESS)
+    if (r != WLM_RESULT_SUCCESS && opcodes.opcode != CMD_AUTH_LOGON_CHALLENGE)
     {
         puts("client opcode read failed");
-        ret = 1;
-        goto exit_goto;
-    }
-
-    if (opcodes.opcode != CMD_AUTH_LOGON_CHALLENGE)
-    {
-        puts("incorrect opcode");
         ret = 1;
         goto exit_goto;
     }
@@ -173,47 +103,21 @@ int main(void)
     c.crc_salt = &crc_salt;
     c.security_flag = VERSION3_SECURITY_FLAG_NONE;
 
-    WowLoginWriter writer = wlm_create_writer(buf, 512);
+    WowLoginWriter writer = wlm_create_writer(buffer, BUFFER_SIZE);
 
     version3_CMD_AUTH_LOGON_CHALLENGE_Server_write(&writer, &c);
-    send(clientfd, (char*)buf, WINDOWS_INT_CAST writer.index, 0);
-
-    bytes = recv(clientfd, (char*)buf, 512, 0);
-    if (bytes == -1)
-    {
-        puts("recv failed");
-        ret = 1;
-        goto exit_goto;
-    }
-
-    reader.index = 0;
-    reader.length = bytes;
+    server_send(&sock, buffer, writer.index);
 
     Version3ClientOpcodeContainer v3_opcodes;
-    r = version3_client_opcode_read(&reader, &v3_opcodes);
-    if (r != WLM_RESULT_SUCCESS)
-    {
-        puts("failed to read opcode v3");
-        ret = 1;
-        goto exit_goto;
-    }
+    expect_version3_opcode(&reader, &sock, &v3_opcodes, CMD_AUTH_LOGON_PROOF);
 
-    if (v3_opcodes.opcode != CMD_AUTH_LOGON_PROOF)
-    {
-        printf("incorrect opcode received %u", v3_opcodes.opcode);
-        ret = 1;
-        goto exit_goto;
-    }
-
-    puts("last one 1");
-
+    puts("received logon proof");
 
     uint8_t server_proof[WOW_SRP_PROOF_LENGTH];
     WowSrpServer* server = wow_srp_proof_into_server(
         proof, (uint8_t*)v3_opcodes.body.CMD_AUTH_LOGON_PROOF_Client.client_public_key,
         (uint8_t*)v3_opcodes.body.CMD_AUTH_LOGON_PROOF_Client.client_proof, server_proof, &error);
 
-    puts("last one 1");
     if (server == NULL)
     {
         printf("server is NULL");
@@ -221,40 +125,31 @@ int main(void)
         goto exit_goto;
     }
 
-    puts("last one 1");
-    version3_CMD_AUTH_LOGON_PROOF_Server s;
-    s.result = VERSION2_LOGIN_RESULT_SUCCESS;
-    s.server_proof = (uint8_t(*)[20])server_proof;
-    s.hardware_survey_id = 0;
+    puts("server is not null");
+    Version3ServerOpcodeContainer s;
+    s.opcode = CMD_AUTH_LOGON_PROOF;
+    s.body.CMD_AUTH_LOGON_PROOF_Server.result = VERSION2_LOGIN_RESULT_SUCCESS;
+    s.body.CMD_AUTH_LOGON_PROOF_Server.server_proof = (uint8_t(*)[20])server_proof;
+    s.body.CMD_AUTH_LOGON_PROOF_Server.hardware_survey_id = 0;
 
-    writer.index = 0;
+    write_version3_opcode(&writer, &sock, &s);
+    puts("Sent logon proof");
 
-    version2_CMD_AUTH_LOGON_PROOF_Server_write(&writer, &s);
-    send(clientfd, (char*)buf, WINDOWS_INT_CAST writer.index, 0);
-    puts("last one 1");
+    expect_version3_opcode(&reader, &sock, &v3_opcodes, CMD_REALM_LIST);
+    puts("Received realm list");
 
-    bytes = recv(clientfd, (char*)buf, 512, 0);
-    if (bytes == -1)
-    {
-        puts("recv failed");
-        ret = 1;
-        goto exit_goto;
-    }
-    puts("last one 1");
+    s.body.CMD_REALM_LIST_Server.number_of_realms = 1;
+    s.body.CMD_REALM_LIST_Server.realms = &realms;
 
-    for (int i = 0; i < bytes; ++i)
-    {
-        printf("%02x ", buf[i]);
-    }
-    puts("last one 1");
+    write_version3_opcode(&writer, &sock, &s);
+    puts("sent realm list");
 
     while (true)
     {
     }
 
 exit_goto:
+    free_socket(&sock);
     all_client_opcode_free(&opcodes);
-    close(sockfd);
-    close(clientfd);
     return ret;
 }
